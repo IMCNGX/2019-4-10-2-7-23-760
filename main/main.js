96959dbd14c0e6c0a05ace5fc884367038775121
IMCNGX <42267896+IMCNGX@users.noreply.github.com>
2019-05-26 00:33:11 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    let totalPrice = 6.0;
+    const timesOverBaseKilometer = 0.5;
+    const baseKilometer = 2;
+    const separateKilometer = 8;
+    const pricePerKilometer = 0.8;
+    const pricePerWaitMinute = 0.25;
+
+    if (distance > 2 && distance <= 8) {
+        totalPrice += pricePerKilometer * (distance - baseKilometer)
+    }
+    if (distance > 8) {
+        totalPrice += pricePerKilometer * (separateKilometer - baseKilometer) + pricePerKilometer * (1 + timesOverBaseKilometer) * (distance - separateKilometer);
+    }
+    totalPrice += pricePerWaitMinute * parkTime;
+    return Math.round(totalPrice);
+    //return "price";
 };
