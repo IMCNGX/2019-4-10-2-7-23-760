aa202118abb2cd5ff294be7918f22e425e599101
IMCNGX <42267896+IMCNGX@users.noreply.github.com>
2019-05-26 00:50:08 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,17 @@
-module.exports = function main(inputs) {
-    let totalPrice = 6.0;
-    const timesOverBaseKilometer = 0.5;
-    const baseKilometer = 2;
-    const separateKilometer = 8;
-    const pricePerKilometer = 0.8;
-    const pricePerWaitMinute = 0.25;
+function taxiFee(distance,waiting) {
+    let result = 6;
+    if(waiting>0){
+        result += waiting * 0.25;
+    }
+    if(distance > 2){
+        if(distance <= 8){
+            result += (distance - 2) * 0.8;
+        } else {
+            result += (8 - 2) * 0.8 + (distance - 8) * 0.8 * 1.5;
+        }
+    }
+    return Math.round(result);
 
-    if (distance > 2 && distance <= 8) {
-        totalPrice += pricePerKilometer * (distance - baseKilometer)
-    }
-    if (distance > 8) {
-        totalPrice += pricePerKilometer * (separateKilometer - baseKilometer) + pricePerKilometer * (1 + timesOverBaseKilometer) * (distance - separateKilometer);
-    }
-    totalPrice += pricePerWaitMinute * parkTime;
-    return Math.round(totalPrice);
-    //return "price";
-};
+}
+
+module.exports = taxiFee;
